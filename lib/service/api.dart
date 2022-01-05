f16a9a4192bba89fd343874766143d8d565f49ad
Abdullah-mamdouh <67171486+Abdullah-mamdouh@users.noreply.github.com>
2022-01-05 13:43:44 +0200

    Update api.dart

--- lib/service/api.dart
+++ lib/service/api.dart
@@ -7,12 +7,12 @@ import 'package:http/http.dart' as http;
 Future<PhotosModel> getRandomWallpaper(int per_page, String keywords, int page_no) async {
   Map<String, String> params = {'per_page': per_page.toString(), 'query': keywords, 'page': page_no.toString()};
 
-  /*
-  * apiKEY : Follow the documentation to collect the api Key from https://www.pexels.com/api/
-  * */
+  
+  // apiKEY : Follow the documentation to collect the api Key from https://www.pexels.com/api/
+  
 
   final response =
-      await http.get(Uri.https('api.pexels.com', 'v1/search', params), headers: {"Authorization": '563492ad6f917000010000015fe4eddae782456a8d1783a3951766de'});
+      await http.get(Uri.https('api.pexels.com', 'v1/search', params), headers: {"Authorization": 'apiKEY'});
 
   if (response.statusCode == 200) {
 
